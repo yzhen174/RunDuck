a64a381112fba7e49ff3c43b528b9faaec252ba6
Yuhui Zhen <yzhen174@ucsc.edu>
2026-06-06 20:17:03 -0700

    update project

---
+++ logs/log.gd
@@ -0,0 +1,23 @@
+extends Area2D
+@onready var logs: Sprite2D = $log
+var speed: float = 200.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y += speed*delta
+	if position.y >1000:
+		queue_free()
+func _on_body_entered(body: Node2D) -> void:	
+	if body.name == "player":
+		if "is_jumping" in body and body.is_jumping == true:
+			return
+		if body.has_method("hide"):
+			body.hide()
+			await get_tree().create_timer(0.1).timeout
+		var main_map = get_tree().current_scene
+		if main_map and main_map.has_method("trigger_game_over"):
+			main_map.trigger_game_over() 
--- map/map.gd
+++ map/map.gd
@@ -1,18 +1,20 @@
 extends Node2D
 @onready var score_label: Label = $CanvasLayer/score
 @onready var parallax_map: Parallax2D = $maps
-
+var whirlpool = preload("res://whirlpoll/whirlpool.tscn")
 var obstacle_scene = preload("res://obstrucle/obstrucle.tscn")
+var rock = preload("res://rocky/rocky.tscn")
+var logg = preload("res://logs/log.tscn")
 var lanes = [-400, 0, 410]
 @onready var GG: Control = $"CanvasLayer/Game over and restart"
 @onready var Start: Control =$"CanvasLayer/GameStart"
-var is_Start: bool = false
 var speed: float = 200.0
-var max: float =800.0
+var max_speed: float =800.0
 @export var speed_increase_rate: float = 10.0
 var score: float = 0.0
 @export var base: float = 1.0
 var is_GG: bool = false
+var is_Start: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -23,7 +25,7 @@ func _process(delta: float) -> void:
 		return
 	if is_GG:
 		return
-	if speed < max:
+	if speed < max_speed:
 		speed += speed_increase_rate * delta
 	if parallax_map:
 		parallax_map.autoscroll.y= speed
@@ -31,17 +33,17 @@ func _process(delta: float) -> void:
 	score += (base * score_multi)*delta
 	if score_label:
 		score_label.text = "score: " + str(int(score))
-func _on_timer_timeout():
+func _on_shark_timeout():
 	if not is_Start:
 		return
-	if obstacle_scene:
-		var new_shark = obstacle_scene.instantiate()
-		
-		var random_lane_index = randi() % 3
-		new_shark.position.x =lanes[random_lane_index]
-		new_shark.position.y = -200
-		if parallax_map:
-			parallax_map.add_child(new_shark)
+	var obstacle = [whirlpool, obstacle_scene, rock, logg]
+	var random_obstacle = obstacle[randi() % obstacle.size()]
+	if random_obstacle:
+		var new_trap = random_obstacle.instantiate()
+		var random_lane = randi() % 3
+		new_trap.position.x = lanes[random_lane]
+		new_trap.position.y = -200
+		add_child(new_trap)
 func trigger_game_start():
 	if is_Start:
 		return
